9dce07606c38fd93de78e1443c225b8fa343e737
eze-une <mruniverse1x@gmail.com>
2022-01-23 23:02:18 +0300

    added the products route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
-
+import Items from './components/items/Items'
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path='/Products' element={<Items />} />
     </Routes>
   );
 }
