31ae4f8502917cc085bcf78385825291bf802cb2
eze-une <mruniverse1x@gmail.com>
2022-01-23 23:03:43 +0300

    added the Item component

---
+++ src/pages/products/Products.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Items from "../../components/items/Items";
+function Products() {
+  return (
+    <>
+      <div style={{ margin: "4rem" }}>
+        <Items />
+      </div>
+    </>
+  );
+}
+
+export default Products;
