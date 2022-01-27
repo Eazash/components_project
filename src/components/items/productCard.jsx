cc88fa6bb37a27bea30fc5420d99116bf83ac117
eze-une <mruniverse1x@gmail.com>
2022-01-27 15:13:45 +0300

    changed to props

--- src/components/items/productCard.jsx
+++ src/components/items/productCard.jsx
@@ -1,24 +1,24 @@
 import { Button, Card } from "react-bootstrap";
 import "./productCard.css";
 
-export default function ProductCard({product,postDelete}) {
+export default function ProductCard(props) {
   const onDelete =id=>e => {
-    postDelete(id); 
+    props.postDelete(id); 
   };
   return (
-    <Card className="card" key={product.id}>
+    <Card className="card" key={props.product.id}>
       <Card.Img
         variant="top"
-        src={product.image}
+        src={props.product.image}
         style={{ borderRadius: "30px 30px 0 0" }}
       />
       <Card.Body>
-        <Card.Title>{product.name}</Card.Title>
-        <Card.Text>{product.description}</Card.Text>
+        <Card.Title>{props.product.name}</Card.Title>
+        <Card.Text>{props.product.description}</Card.Text>
         <Card.Text style={{ fontSize: "2rem", color: "blue" }}>
-          {product.price}$
+          {props.product.price}$
         </Card.Text>
-        <Button color="error" onClick={onDelete(product.id)}>
+        <Button color="error" onClick={onDelete(props.product.id)}>
           Delete
         </Button>
       </Card.Body>
