d1d6285e1cffce2f02363bba8871a8e7c48cf72e
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-01-27 18:47:24 +0300

    Clean up userRouter.js

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getUsers } = require("../controllers/userController");
+const { getUsers } = require("../controllers/user/userController");
 const router = express.Router();
 
 router.get("/", getUsers);
