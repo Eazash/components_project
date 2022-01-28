7621124ce0db654020a84fa79964fe6122c945a2
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-01-28 17:50:55 +0300

    Added login and logout endpoint

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,5 +1,7 @@
 const { Schema, model } = require("mongoose");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const config = require("../config/config");
 const salt = 10;
 
 const UserSchema = new Schema({
@@ -42,5 +44,43 @@ UserSchema.pre("save", function (next) {
 		next();
 	}
 });
+
+UserSchema.methods.comparePassword = function (password, cb, next) {
+	bcrypt.compare(password, this.password, function (err, isMatch) {
+		if (err) return cb(next);
+		cb(null, isMatch);
+	});
+};
+
+UserSchema.methods.generateToken = function (cb) {
+	var user = this;
+	var token = jwt.sign(user._id.toHexString(), config.secret);
+
+	user.token = token;
+	user.save(function (err, user) {
+		if (err) return cb(err);
+		cb(null, user);
+	});
+};
+
+UserSchema.statics.findByToken = function (token, cb) {
+	var user = this;
+
+	jwt.verify(token, config.secret, function (err, decode) {
+		user.findOne({ _id: decode, token: token }, function (err, user) {
+			if (err) return cb(err);
+			cb(null, user);
+		});
+	});
+};
+
+UserSchema.methods.deleteToken = function (token, cb) {
+	var user = this;
+
+	user.update({ $unset: { token: 1 } }, function (err, user) {
+		if (err) return cb(err);
+		cb(null, user);
+	});
+};
 
 module.exports = new model("User", UserSchema);
