ea27db475725a83ada9441998f913b30d824614b
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-01-28 15:23:35 +0300

    Added hashing function

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,23 +1,46 @@
 const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+const salt = 10;
 
 const UserSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
+	username: {
+		type: String,
+		required: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+	},
+	token: {
+		type: String,
+	},
+});
+
+UserSchema.pre("save", function (next) {
+	var user = this;
+
+	if (user.isModified("password")) {
+		bcrypt.genSalt(salt, function (err, salt) {
+			if (err) return next(err);
+
+			bcrypt.hash(user.password, salt, function (err, hash) {
+				if (err) return next(err);
+				user.password = hash;
+				next();
+			});
+		});
+	} else {
+		next();
+	}
 });
 
 module.exports = new model("User", UserSchema);
