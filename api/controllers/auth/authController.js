f2cc0811bac508c7f944811595894279313791b6
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-01-28 15:28:02 +0300

    Added signup route

--- api/controllers/auth/authController.js
+++ api/controllers/auth/authController.js
@@ -1,3 +1,21 @@
+const User = require("../../config/database").User;
+
 module.exports.signupUser = function (req, res) {
-	return res.send("Signup User");
+	const newUser = new User(req.body);
+
+	User.findOne({ email: newUser.email, username: newUser.username }, function (err, user) {
+		if (user)
+			return res.status(400).json({ auth: false, message: "Email and Username already exists" });
+
+		newUser.save((err, doc) => {
+			if (err) {
+				console.log(err);
+				return res.status(400).json({ success: false });
+			}
+			res.status(200).json({
+				success: true,
+				user: doc,
+			});
+		});
+	});
 };
