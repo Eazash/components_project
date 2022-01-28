b89d54a9ab7e2b4eff7c0975f15c1ed613469554
Ezra Ashenafi <eazash22@gmail.com>
2022-01-28 15:26:22 +0300

    register product routes

--- api/routes/main.js
+++ api/routes/main.js
@@ -1,5 +1,7 @@
 const userRouter = require("./userRouter");
+const productRouter = require("./productRouter");
 
 module.exports.init = function (app) {
   app.use("/api/users", userRouter);
+  app.use("/api/products", productRouter);
 };
