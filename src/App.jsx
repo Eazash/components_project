518c5013b6f7c07847e316290276973fd2f3ee69
eze-une <mruniverse1x@gmail.com>
2022-01-29 20:05:21 +0300

    added a route to update component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Home from "./pages/home";
 import Items from './components/items/Items'
+import UpdateForm from "./pages/updateform";
 function App() {
   return (
     <div>
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/" element={<Home />} />
         <Route path='/Products' element={<Items />} />
+        <Route path='/Products/update' element={<UpdateForm />} />
       </Routes>
     </div>
   );
