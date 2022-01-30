1c8c0a4b5cf4c18567c4b745af6c73950cd97218
eze-une <mruniverse1x@gmail.com>
2022-01-30 20:43:19 +0300

    made a list of chckout Items

---
+++ src/components/checkout/checkoutlist.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+import './checkout.css'
+
+function CheckoutList({item}) {
+  return (
+    <div>
+      <Card className="card" key={item.id} style={{margin: '0'}}>
+          <div className="card-layout">
+        <Card.Img
+          variant="top"
+          src={item.image}
+          style={{height:'200px', width:'400px'}}
+        />
+        <Card.Body>
+          <Card.Title>{item.name}</Card.Title>
+          <Card.Text>{item.description}</Card.Text>
+          <Card.Text style={{ fontSize: "2rem", color: "blue" }}>
+            {item.price}$
+          </Card.Text>
+        </Card.Body>
+        <Button>Delete</Button>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+export default CheckoutList;
