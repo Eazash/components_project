8b92b4361e0b7c1bf29b547ef73666e00d65c23b
eze-une <mruniverse1x@gmail.com>
2022-01-30 20:43:55 +0300

    imported chekoutList components

--- src/components/checkout/index.jsx
+++ src/components/checkout/index.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+import { useStateValue } from "../../StateProvider";
+import CheckoutList from "./checkoutlist";
+import { Row, Col } from "react-bootstrap";
+import "./checkout.css";
 
 function Checkout() {
-  return <div></div>;
+  const [{ basket }] = useStateValue();
+  return (
+    <>
+      <Row xs={1} md={1} className="g-4">
+        {basket.map((item, idx) => (
+          <Col>
+            <div>
+              <CheckoutList item={item} key={idx} />
+            </div>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
 
 export default Checkout;
