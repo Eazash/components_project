8d877e4c9d77f6eb775d6994ec2913099ae5e201
Ezra Ashenafi <eazash22@gmail.com>
2022-01-31 01:36:53 +0300

    create product store

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
+import productReducer from "./productSlice";
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    products: productReducer,
+  },
 });
