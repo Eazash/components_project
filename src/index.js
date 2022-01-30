01da9e9a9fc6ec8ca8ee470368b034307d55affe
eze-une <mruniverse1x@gmail.com>
2022-01-30 18:41:21 +0300

    wrapped the app with stateprovider

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import "./variables.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { StateProvider } from "./StateProvider";
+import {initialState} from './reducer';
+import reducer from "./reducer";
 
 ReactDOM.render(
   <BrowserRouter>
+  <StateProvider initialState={initialState} reducer={reducer }>
     <App />
+    </StateProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
