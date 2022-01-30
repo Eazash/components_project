f4198b0df88a85ffe0a643e10489eb1ff4964e89
eze-une <mruniverse1x@gmail.com>
2022-01-30 18:40:56 +0300

    added to checkout page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Home from "./pages/home";
 import Items from './components/items/Items'
+import Checkout from "./components/checkout";
 function App() {
   return (
     <div>
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/" element={<Home />} />
         <Route path='/Products' element={<Items />} />
+        <Route path='/checkout' element={<Checkout />}/>
       </Routes>
     </div>
   );
