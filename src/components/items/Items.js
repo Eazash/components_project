e57889299634575ab43b34bcfb4319003ec59e3a
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-01-31 10:17:32 +0300

    Removed unused useState

--- src/components/items/Items.js
+++ src/components/items/Items.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import axios from "axios";
 import ProductCard from "./productCard";
