b49f94c3fbd299579b55cbd47432d0cd99e26370
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-01-31 10:42:59 +0300

    Added login

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,10 +6,10 @@ export default function NavBar() {
 	return (
 		<Navbar bg="warning" className="navbar">
 			<Container>
-				<Navbar.Brand href="#home" className=".align-middle">
-					UPSHOP
-				</Navbar.Brand>
-				<Nav className="me-auto .align-middle">
+				<LinkContainer to="/">
+					<Navbar.Brand>UPSHOP</Navbar.Brand>
+				</LinkContainer>
+				<Nav>
 					<LinkContainer to="/">
 						<Nav.Link>Home</Nav.Link>
 					</LinkContainer>
@@ -18,6 +18,9 @@ export default function NavBar() {
 					</LinkContainer>
 				</Nav>
 				<Nav className="d-flex">
+					<LinkContainer to="/login">
+						<Nav.Link>Log In</Nav.Link>
+					</LinkContainer>
 					<Button variant="light">
 						Cart <Badge bg="dark">9</Badge>
 						<span className="visually-hidden">cart</span>
