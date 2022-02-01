6b9e88728dbca6c4927f92422384008ea99e7d6b
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-02-01 15:05:39 +0300

    Added auth routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
-import Items from '../components/items/Items'
+import Items from "../components/items/Items";
+import Login from "../pages/auth/Login";
+import Signup from "../pages/auth/Signup";
 
 function AllRoutes() {
-  return (
-	<Routes>
-	<Route path="/" element={<Home />} />
-	<Route path='/products' element={<Items />} />
-  </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/products" element={<Items />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+		</Routes>
+	);
 }
 
 export default AllRoutes;
