d976714052ae156b39efbe0f7c948bfc938f0877
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-02-01 17:27:46 +0300

    Hide navbar on login and signup

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,13 +5,20 @@ import Items from "../components/items/Items";
 import Login from "../pages/auth/Login";
 import Signup from "../pages/auth/Signup";
 
+import WithNav from "./WithNav";
+import WithoutNav from "./WithoutNav";
+
 function AllRoutes() {
 	return (
 		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/products" element={<Items />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
+			<Route element={<WithNav />}>
+				<Route path="/" element={<Home />} />
+				<Route path="/products" element={<Items />} />
+			</Route>
+			<Route element={<WithoutNav />}>
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<Signup />} />
+			</Route>
 		</Routes>
 	);
 }
