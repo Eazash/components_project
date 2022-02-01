dd185aa6027b8b00c8804522e2bd07122231675b
Hiwot Medhin <hiwotmedhings@gmail.com>
2022-02-01 22:57:34 +0300

    Fix link

--- src/components/home/HomeCards.jsx
+++ src/components/home/HomeCards.jsx
@@ -1,19 +1,23 @@
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Nav } from "react-bootstrap";
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { LinkContainer } from "react-router-bootstrap";
 
 function HomeCards(props) {
 	return (
 		<div>
 			<Card className="home-card">
-				<Card.Img className="home-card-img" variant="top" src={props.product.image} />
+				<Card.Img
+					className="home-card-img"
+					variant="top"
+					src={props.product.image}
+				/>
 				<Card.Body>
 					<Card.Title>{props.product.name}</Card.Title>
-					<Card.Text>
-                    {props.product.description}
-					</Card.Text>
-					<Button variant="warning" className="rounded-pill" onClick={<Navigate to="/login" />}>
-						Buy Now
+					<Card.Text>{props.product.description}</Card.Text>
+					<Button variant="warning" className="rounded-pill">
+						<LinkContainer to="/products">
+							<Nav.Link className="home-link">Buy Now</Nav.Link>
+						</LinkContainer>
 					</Button>
 				</Card.Body>
 			</Card>
