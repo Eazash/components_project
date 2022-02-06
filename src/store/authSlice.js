9957bdb165659f85f71440481434674d4c682ba4
Ezra Ashenafi <eazash22@gmail.com>
2022-02-07 01:51:15 +0300

    add auth store

---
+++ src/store/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  id: null,
+  email: null,
+  username: null,
+  isAdmin: false,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    saveUser: (state, action) => {
+      const { id, email, username, isAdmin } = action.payload;
+      state.id = id;
+      state.email = email;
+      state.username = username;
+      state.isAdmin = isAdmin;
+    },
+  },
+});
+
+export const { saveUser } = authSlice.actions;
+
+export default authSlice.reducer;
